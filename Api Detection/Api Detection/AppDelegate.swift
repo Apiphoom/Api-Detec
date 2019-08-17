4d7298aacb74290f05f63e5bfebdda9a426d2c94
Apiphoom Chuenchompoo <48949523+Apiphoom@users.noreply.github.com>
2019-08-17 07:02:51 +0700

    Add files via upload

---
+++ Api Detection/Api Detection/AppDelegate.swift
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.swift
+//  Api Detection
+//
+//  Created by Apiphoom Chuenchompoo on 6/8/19.
+//  Copyright © 2019 Apiphoom Chuenchompoo. All rights reserved.
+//
+//api
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+
+}
+
+
+
